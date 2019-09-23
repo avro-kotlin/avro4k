ed657d5dd5ff3310cc098eeedf55de1ff1d4f178
sksamuel <sam@sksamuel.com>
2019-09-22 20:52:05 -0500

    Added support for basic array decoders

--- src/main/kotlin/com/sksamuel/avro4k/RecordDecoder.kt
+++ src/main/kotlin/com/sksamuel/avro4k/RecordDecoder.kt
@@ -1,6 +1,11 @@
 package com.sksamuel.avro4k
 
+import kotlinx.serialization.CompositeDecoder
+import kotlinx.serialization.ElementValueDecoder
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.NamedValueDecoder
+import kotlinx.serialization.SerialDescriptor
+import kotlinx.serialization.StructureKind
 import org.apache.avro.generic.GenericData
 
 class RecordDecoder(val record: GenericData.Record) : NamedValueDecoder() {
@@ -26,6 +31,70 @@ class RecordDecoder(val record: GenericData.Record) : NamedValueDecoder() {
   }
 
   override fun decodeTaggedString(tag: String): String {
+    println(tag)
     return record.get(tag) as String
   }
+
+  var index = 0
+
+  override fun decodeElementIndex(desc: SerialDescriptor): Int {
+    println("decodeElementIndex $desc $index")
+    if (index == desc.elementsCount) return CompositeDecoder.READ_DONE
+    val k = index
+    index++
+    return k
+  }
+
+  private val lists = mutableListOf<Array<Any>>()
+
+  override fun beginStructure(desc: SerialDescriptor, vararg typeParams: KSerializer<*>): CompositeDecoder {
+    println("beginStructure $desc")
+    return when (desc.kind as StructureKind) {
+      StructureKind.CLASS -> this
+      StructureKind.LIST -> {
+        when (val data = record.get(currentTag)) {
+          is List<*> -> ListDecoder(data)
+          is Array<*> -> ListDecoder(data.asList())
+          else -> this
+        }
+      }
+      StructureKind.MAP -> this
+    }
+  }
+
+  override fun endStructure(desc: SerialDescriptor) {
+    println("endStructure $desc")
+    super.endStructure(desc)
+  }
+
+  override fun decodeCollectionSize(desc: SerialDescriptor): Int {
+    return super.decodeCollectionSize(desc)
+  }
+}
+
+class ListDecoder(private val array: List<Any?>) : ElementValueDecoder() {
+
+  private var index = 0
+
+  override fun decodeBoolean(): Boolean {
+    return array[index++] as Boolean
+  }
+
+  override fun decodeLong(): Long {
+    return array[index++] as Long
+  }
+
+  override fun decodeString(): String {
+    return array[index++] as String
+  }
+
+  override fun decodeDouble(): Double {
+    return array[index++] as Double
+  }
+
+  override fun decodeFloat(): Float {
+    return array[index++] as Float
+  }
+
+  override fun decodeCollectionSize(desc: SerialDescriptor): Int = array.size
 }
---
+++ src/test/kotlin/com/sksamuel/avro4k/decoder/ArrayDecoderTest.kt
@@ -0,0 +1,187 @@
+package com.sksamuel.avro4k.decoder
+
+import com.sksamuel.avro4k.Avro
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.WordSpec
+import kotlinx.serialization.Serializable
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+
+@Serializable
+data class TestArrayBooleans(val booleans: Array<Boolean>)
+
+@Serializable
+data class TestListDoubles(val doubles: List<Double>)
+
+@Serializable
+data class TestSetString(val strings: Set<String>)
+
+@Serializable
+data class TestArrayRecords(val records: Array<Record>)
+
+@Serializable
+data class TestListRecords(val records: List<Record>)
+
+@Serializable
+data class TestSetRecords(val records: Set<Record>)
+
+@Serializable
+data class Record(val str: String, val double: Double)
+
+class ArrayDecoderTest : WordSpec({
+
+  "Decoder" should {
+
+    "support array for an Array of booleans" {
+      val schema = Avro.default.schema(TestArrayBooleans.serializer())
+      val record = GenericData.Record(schema)
+      record.put("booleans", arrayOf(true, false, true))
+      Avro.default.fromRecord(TestArrayBooleans.serializer(), record).booleans.toList() shouldBe
+          listOf(true, false, true)
+    }
+
+    "support list for an Array of booleans" {
+      val schema = Avro.default.schema(TestArrayBooleans.serializer())
+      val record = GenericData.Record(schema)
+      record.put("booleans", listOf(true, false, true))
+      Avro.default.fromRecord(TestArrayBooleans.serializer(), record).booleans.toList() shouldBe
+          listOf(true, false, true)
+    }
+
+    "support GenericData.Array for an Array of booleans" {
+      val schema = Avro.default.schema(TestArrayBooleans.serializer())
+      val record = GenericData.Record(schema)
+      record.put("booleans",
+          GenericData.Array(Schema.createArray(Schema.create(Schema.Type.BOOLEAN)), listOf(true, false, true)))
+      Avro.default.fromRecord(TestArrayBooleans.serializer(), record).booleans.toList() shouldBe
+          listOf(true, false, true)
+    }
+
+    "support array for a List of doubles" {
+
+      val schema = Avro.default.schema(TestListDoubles.serializer())
+      val record = GenericData.Record(schema)
+      record.put("doubles", arrayOf(12.54, 23.5, 9123.2314))
+      Avro.default.fromRecord(TestListDoubles.serializer(), record) shouldBe
+          TestListDoubles(listOf(12.54, 23.5, 9123.2314))
+    }
+
+    "support list for a List of doubles" {
+
+      val schema = Avro.default.schema(TestListDoubles.serializer())
+      val record = GenericData.Record(schema)
+      record.put("doubles", listOf(12.54, 23.5, 9123.2314))
+      Avro.default.fromRecord(TestListDoubles.serializer(), record) shouldBe
+          TestListDoubles(listOf(12.54, 23.5, 9123.2314))
+    }
+
+    "support GenericData.Array for a List of doubles" {
+
+      val schema = Avro.default.schema(TestListDoubles.serializer())
+      val record = GenericData.Record(schema)
+      record.put("doubles",
+          GenericData.Array(Schema.createArray(Schema.create(Schema.Type.DOUBLE)), listOf(12.54, 23.5, 9123.2314)))
+      Avro.default.fromRecord(TestListDoubles.serializer(), record) shouldBe
+          TestListDoubles(listOf(12.54, 23.5, 9123.2314))
+    }
+//
+//    "support array for a List of records" {
+//
+//      val containerSchema = Avro.default.schema(TestListRecords.serializer())
+//      val recordSchema = Avro.default.schema(Record.serializer())
+//
+//      val record1 = GenericData.Record(recordSchema)
+//      record1.put("str", "qwe")
+//      record1.put("double", 123.4)
+//
+//      val record2 = GenericData.Record(recordSchema)
+//      record2.put("str", "wer")
+//      record2.put("double", 8234.324)
+//
+//      val container = GenericData.Record(containerSchema)
+//      container.put("records", arrayOf(record1, record2))
+//
+//      //Decoder[TestListRecords].decode(container, containerSchema, DefaultFieldMapper) shouldBe TestListRecords(List(Record("qwe", 123.4), Record("wer", 8234.324)))
+//    }
+//
+//    "support array for an Array of records" {
+//
+//      val containerSchema = Avro.default.schema(TestArrayRecords.serializer())
+//      val recordSchema = Avro.default.schema(Record.serializer())
+//
+//      val record1 = GenericData.Record(recordSchema)
+//      record1.put("str", "qwe")
+//      record1.put("double", 123.4)
+//
+//      val record2 = GenericData.Record(recordSchema)
+//      record2.put("str", "wer")
+//      record2.put("double", 8234.324)
+//
+//      val container = GenericData.Record(containerSchema)
+//      container.put("records", arrayOf(record1, record2))
+//
+//      //Decoder[TestArrayRecords].decode(container, containerSchema, DefaultFieldMapper).records.toVector shouldBe Vector(Record("qwe", 123.4), Record("wer", 8234.324))
+//    }
+//
+//    "support array for a Set of records" {
+//
+//      val containerSchema = Avro.default.schema(TestSetRecords.serializer())
+//      val recordSchema = Avro.default.schema(Record.serializer())
+//
+//      val record1 = GenericData.Record(recordSchema)
+//      record1.put("str", "qwe")
+//      record1.put("double", 123.4)
+//
+//      val record2 = GenericData.Record(recordSchema)
+//      record2.put("str", "wer")
+//      record2.put("double", 8234.324)
+//
+//      val container = GenericData.Record(containerSchema)
+//      container.put("records", arrayOf(record1, record2))
+//
+//      //Decoder[TestSetRecords].decode(container, containerSchema, DefaultFieldMapper) shouldBe TestSetRecords(Set(Record("qwe", 123.4), Record("wer", 8234.324)))
+//    }
+
+    "support array for a Set of strings" {
+      val schema = Avro.default.schema(TestSetString.serializer())
+      val record = GenericData.Record(schema)
+      record.put("strings", arrayOf("Qwe", "324", "q"))
+      Avro.default.fromRecord(TestSetString.serializer(), record) shouldBe
+          TestSetString(setOf("Qwe", "324", "q"))
+    }
+
+    "support list for a Set of strings" {
+      val schema = Avro.default.schema(TestSetString.serializer())
+      val record = GenericData.Record(schema)
+      record.put("strings", arrayOf("Qwe", "324", "q"))
+      Avro.default.fromRecord(TestSetString.serializer(), record) shouldBe
+          TestSetString(setOf("Qwe", "324", "q"))
+    }
+
+    "support GenericData.Array for a Set of strings" {
+      val schema = Avro.default.schema(TestSetString.serializer())
+      val record = GenericData.Record(schema)
+      record.put("strings",
+          GenericData.Array(Schema.createArray(Schema.create(Schema.Type.STRING)), listOf("Qwe", "324", "q")))
+      Avro.default.fromRecord(TestSetString.serializer(), record) shouldBe
+          TestSetString(setOf("Qwe", "324", "q"))
+    }
+//    //    "support Seq[Tuple2] issue #156" {
+//    //      val schema = SchemaEncoder[TupleTest2]
+//    //    }
+//    //    "support Seq[Tuple3]" {
+//    //      val schema = SchemaEncoder[TupleTest3]
+//    //    }
+////
+////    "support top level Seq<Double>" {
+////      //Decoder[Seq<Double>].decode(Array(1.2, 34.5, 54.3), Avro.default.schema(Seq[Double.serializer())], DefaultFieldMapper) shouldBe Seq(1.2, 34.5, 54.3)
+////    }
+////    "support top level List[Int]" {
+////      //Decoder[List[Int]].decode(Array(1, 4, 9), Avro.default.schema(Seq[Int.serializer())], DefaultFieldMapper) shouldBe List(1, 4, 9)
+////    }
+////    "support top level Set<Boolean>" {
+////      //Decoder[Set<Boolean>].decode(Array(true, false, true), Avro.default.schema(Seq[Boolean.serializer())], DefaultFieldMapper) shouldBe Set(true, false)
+////    }
+  }
+
+})

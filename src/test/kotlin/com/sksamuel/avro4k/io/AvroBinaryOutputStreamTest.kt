d8ffd560a232020827592ce0d31722dba4222137
sksamuel <sam@sksamuel.com>
2019-10-06 15:49:46 -0500

    Added test that binary stream doesn't include schema

---
+++ src/test/kotlin/com/sksamuel/avro4k/io/AvroBinaryOutputStreamTest.kt
@@ -0,0 +1,38 @@
+package com.sksamuel.avro4k.io
+
+import com.sksamuel.avro4k.Avro
+import io.kotlintest.inspectors.forNone
+import io.kotlintest.matchers.string.shouldContain
+import io.kotlintest.specs.StringSpec
+import kotlinx.serialization.Serializable
+import java.io.ByteArrayOutputStream
+
+class AvroBinaryOutputStreamTest : StringSpec({
+
+   @Serializable
+   data class Work(val name: String, val year: Int)
+
+   @Serializable
+   data class Composer(val name: String, val birthplace: String, val works: List<Work>)
+
+   val ennio = Composer("ennio morricone", "rome", listOf(Work("legend of 1900", 1986), Work("ecstasy of gold", 1969)))
+
+   val hans = Composer("hans zimmer", "frankfurt", listOf(Work("batman begins", 2007), Work("dunkirk", 2017)))
+
+   "AvroBinaryOutputStream should not write schemas"  {
+
+      val schema = Avro.default.schema(Composer.serializer())
+
+      val baos = ByteArrayOutputStream()
+      val output = AvroOutputStream.binary(schema, Composer.serializer()).to(baos)
+      output.write(ennio)
+      output.write(hans)
+      output.close()
+
+      // the schema should not be written in a binary stream
+      listOf("name", "birthplace", "works", "year").forNone {
+         String(baos.toByteArray()).shouldContain(it)
+      }
+   }
+
+})

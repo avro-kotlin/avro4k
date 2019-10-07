fd521e1ad27db66d581c313bff0312244a48f72e
sksamuel <sam@sksamuel.com>
2019-10-07 06:36:46 -0500

    Added tests for nullable dates

--- src/test/kotlin/com/sksamuel/avro4k/encoder/DateEncoderTest.kt
+++ src/test/kotlin/com/sksamuel/avro4k/encoder/DateEncoderTest.kt
@@ -30,17 +30,27 @@ class DateEncoderTest : FunSpec({
    test("encode LocalTime as an Int") {
 
       @Serializable
-      data class Foo(val s: LocalTime)
+      data class Foo(val t: LocalTime)
 
       val schema = Avro.default.schema(Foo.serializer())
       Avro.default.toRecord(Foo.serializer(), Foo(LocalTime.of(12, 50, 45))) shouldBe
          ListRecord(schema, 46245000)
    }
 
+   test("encode nullable LocalTime") {
+
+      @Serializable
+      data class Foo(val t: LocalTime?)
+
+      val schema = Avro.default.schema(Foo.serializer())
+      Avro.default.toRecord(Foo.serializer(), Foo(LocalTime.of(12, 50, 45))) shouldBe ListRecord(schema, 46245000)
+      Avro.default.toRecord(Foo.serializer(), Foo(null)) shouldBe ListRecord(schema, null)
+   }
+
    test("encode LocalDate as an Int") {
 
       @Serializable
-      data class Foo(val s: LocalDate)
+      data class Foo(val d: LocalDate)
 
       val schema = Avro.default.schema(Foo.serializer())
       Avro.default.toRecord(Foo.serializer(), Foo(LocalDate.of(2018, 9, 10))) shouldBe
@@ -50,7 +60,7 @@ class DateEncoderTest : FunSpec({
    test("encode LocalDateTime as Long") {
 
       @Serializable
-      data class Foo(val s: LocalDateTime)
+      data class Foo(val dt: LocalDateTime)
 
       val schema = Avro.default.schema(Foo.serializer())
       Avro.default.toRecord(Foo.serializer(), Foo(LocalDateTime.of(2018, 9, 10, 11, 58, 59))) shouldBe
@@ -60,9 +70,20 @@ class DateEncoderTest : FunSpec({
    test("encode Timestamp as Long") {
 
       @Serializable
-      data class Foo(val s: Timestamp)
+      data class Foo(val t: Timestamp)
 
       val schema = Avro.default.schema(Foo.serializer())
+      Avro.default.toRecord(Foo.serializer(), Foo(Timestamp.from(Instant.ofEpochMilli(1538312231000L)))) shouldBe
+         ListRecord(schema, 1538312231000L)
+   }
+
+   test("encode nullable Timestamp as Long") {
+
+      @Serializable
+      data class Foo(val t: Timestamp?)
+
+      val schema = Avro.default.schema(Foo.serializer())
+      Avro.default.toRecord(Foo.serializer(), Foo(null)) shouldBe ListRecord(schema, null)
       Avro.default.toRecord(Foo.serializer(), Foo(Timestamp.from(Instant.ofEpochMilli(1538312231000L)))) shouldBe
          ListRecord(schema, 1538312231000L)
    }
@@ -70,7 +91,7 @@ class DateEncoderTest : FunSpec({
    test("encode Instant as Long") {
 
       @Serializable
-      data class Foo(val s: Instant)
+      data class Foo(val i: Instant)
 
       val schema = Avro.default.schema(Foo.serializer())
       Avro.default.toRecord(Foo.serializer(), Foo(Instant.ofEpochMilli(1538312231000L))) shouldBe

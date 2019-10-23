0e9b5d88374ab553069acbabd1d9aa2b54b1b597
sksamuel <sam@sksamuel.com>
2019-10-23 16:24:20 -0500

    Added URL serializer

---
+++ avro4k-core/src/main/kotlin/com/sksamuel/avro4k/serializer/URLSerializer.kt
@@ -0,0 +1,34 @@
+package com.sksamuel.avro4k.serializer
+
+import com.sksamuel.avro4k.decoder.ExtendedDecoder
+import com.sksamuel.avro4k.encoder.ExtendedEncoder
+import com.sksamuel.avro4k.schema.AvroDescriptor
+import kotlinx.serialization.PrimitiveKind
+import kotlinx.serialization.SerialDescriptor
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.Serializer
+import org.apache.avro.Schema
+import org.apache.avro.SchemaBuilder
+import org.apache.avro.util.Utf8
+import java.net.URL
+import kotlin.reflect.jvm.jvmName
+
+@Serializer(forClass = URL::class)
+class URLSerializer : AvroSerializer<URL>() {
+
+   override val descriptor: SerialDescriptor = object : AvroDescriptor(URL::class.jvmName, PrimitiveKind.STRING) {
+      override fun schema(annos: List<Annotation>): Schema =  SchemaBuilder.builder().stringType()
+   }
+
+   override fun encodeAvroValue(schema: Schema, encoder: ExtendedEncoder, obj: URL) {
+      encoder.encodeString(obj.toString())
+   }
+
+   override fun decodeAvroValue(schema: Schema, decoder: ExtendedDecoder): URL {
+      return when (val v = decoder.decodeAny()) {
+         is Utf8 -> URL(decoder.decodeString())
+         is String -> URL(v)
+         else -> throw SerializationException("Unsupported URL type [$v]")
+      }
+   }
+}

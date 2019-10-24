401f0f79c7b73bed80113f4e750707fadf76feb5
sksamuel <sam@sksamuel.com>
2019-10-24 08:45:09 -0500

    Release 0.15

--- avro4k-core/src/main/kotlin/com/sksamuel/avro4k/serializer/AvroSerializer.kt
+++ avro4k-core/src/main/kotlin/com/sksamuel/avro4k/serializer/AvroSerializer.kt
@@ -10,7 +10,6 @@ import kotlinx.serialization.Encoder
 import kotlinx.serialization.KSerializer
 import org.apache.avro.Schema
 
-
 abstract class AvroSerializer<T> : KSerializer<T> {
 
    final override fun serialize(encoder: Encoder, obj: T) {

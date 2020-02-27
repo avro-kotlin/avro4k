a06a2485063158cdea627c35f1ae0db948ddee1a
Thorsten Hake <thorsten.hake@dew-stahl.com>
2020-02-27 12:14:01 +0100

    Honoring AvroDoc and AvroAlias / AvroAliases for enum class

--- avro4k-core/src/test/kotlin/com/sksamuel/avro4k/schema/EnumSchemaTest.kt
+++ avro4k-core/src/test/kotlin/com/sksamuel/avro4k/schema/EnumSchemaTest.kt
@@ -4,6 +4,8 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import kotlinx.serialization.Serializable
 import com.sksamuel.avro4k.Avro
+import com.sksamuel.avro4k.AvroAliases
+import com.sksamuel.avro4k.AvroDoc
 
 class EnumSchemaTest : WordSpec({
 
@@ -27,8 +29,23 @@ class EnumSchemaTest : WordSpec({
 //      schema.toString(true) shouldBe expected.toString(true)
 //    }
   }
+   "Enum with documentation and aliases" should{
+      @Serializable
+      data class EnumWithDocuTest(
+         val value : Suit
+      )
+      val expected = org.apache.avro.Schema.Parser().parse(javaClass.getResourceAsStream("/enum_with_default.json"))
+      val schema = Avro.default.schema(EnumWithDocuTest.serializer())
+      schema.toString(true) shouldBe expected.toString(true)
+   }
 })
 
 enum class Wine {
   Malbec, Shiraz, CabSav, Merlot
+}
+@Serializable
+@AvroAliases(["MySuit"])
+@AvroDoc("documentation")
+enum class Suit{
+   SPADES, HEARTS, DIAMONDS, CLUBS;
 }

571ad8192b1642d5094917ad360346d6fe0c09ea
Thorsten Hake <mail@thorsten-hake.com>
2022-01-06 07:02:44 +0100

    Removed native support for sealed interfaces but temporary added helper methods to correctly setup SerializersModule.

--- src/test/kotlin/com/github/avrokotlin/avro4k/schema/SealedInterfaceSchemaTest.kt
+++ src/test/kotlin/com/github/avrokotlin/avro4k/schema/SealedInterfaceSchemaTest.kt
@@ -2,9 +2,12 @@ package com.github.avrokotlin.avro4k.schema
 
 import com.github.avrokotlin.avro4k.Avro
 import com.github.avrokotlin.avro4k.AvroDefault
+import com.github.avrokotlin.avro4k.polymorphicTreeForSealed
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.modules.SerializersModule
 import org.apache.avro.Schema
 
 sealed interface Calculable
@@ -35,16 +38,27 @@ data class ReferencingNullableSealedInterface(
    val nullable : Calculable?
 )
 
+@OptIn(InternalSerializationApi::class)
 class SealedInterfaceSchemaTest : StringSpec({
 
    "referencing a sealed hierarchy"{
+      val schema = Avro(serializersModule = SerializersModule {
+         polymorphicTreeForSealed(Calculable::class)
+      }).schema(ReferencingSealedInterface.serializer())
+      val expected = Schema.Parser().parse(javaClass.getResourceAsStream("/sealed_interface_hierarchy_referenced.json"))
+      schema shouldBe expected
+   }
+   "referencing a nullable sealed hierarchy"{
+      val schema = Avro(serializersModule = SerializersModule {
+         polymorphicTreeForSealed(Calculable::class)
+      }).schema(ReferencingNullableSealedInterface.serializer())
+      val expected = Schema.Parser().parse(javaClass.getResourceAsStream("/sealed_interface_nullable_referenced.json"))
+      schema shouldBe expected
+   }
+   "referencing a sealed interface"{
       val schema = Avro.default.schema(ReferencingSealedInterface.serializer())
       val expected = Schema.Parser().parse(javaClass.getResourceAsStream("/sealed_interface_referenced.json"))
       schema shouldBe expected
    }
-   "referencing a nullable sealed hierarchy"{
-      val schema = Avro.default.schema(ReferencingNullableSealedInterface.serializer())
-      val expected = Schema.Parser().parse(javaClass.getResourceAsStream("/sealed_interface_nullable_referenced.json"))
-      schema shouldBe expected
-   }
 })
+

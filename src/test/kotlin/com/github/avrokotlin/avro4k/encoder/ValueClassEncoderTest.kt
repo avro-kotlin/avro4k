ba3f6a7aa0563a7f9d8d569bceae3744bd11891c
Thorsten Hake <mail@thorsten-hake.com>
2023-04-19 08:10:20 +0200

    wip - adding support for avro primitives as root

--- src/test/kotlin/com/github/avrokotlin/avro4k/encoder/ValueClassEncoderTest.kt
+++ src/test/kotlin/com/github/avrokotlin/avro4k/encoder/ValueClassEncoderTest.kt
@@ -17,38 +17,45 @@ import kotlin.reflect.KProperty1
 
 @OptIn(InternalSerializationApi::class)
 class ValueClassEncoderTest : StringSpec({
-   include(valueClassEncodeTest(StringWrapper("blub")))
-   include(valueClassEncodeTest(IntWrapper(1)))
-   include(valueClassEncodeTest(LongWrapper(1L)))
-   include(valueClassEncodeTest(ByteWrapper(1)))
-   include(valueClassEncodeTest(ShortWrapper(1)))
-   include(valueClassEncodeTest(BooleanWrapper(true)))
-   include(valueClassEncodeTest(FloatWrapper(1f)))
-   include(valueClassEncodeTest(DoubleWrapper(1.02)))   
-   "encode referenced value class" {
+    include(valueClassEncodeTest(StringWrapper("blub")))
+    include(valueClassEncodeTest(IntWrapper(1)))
+    include(valueClassEncodeTest(LongWrapper(1L)))
+    include(valueClassEncodeTest(ByteWrapper(1)))
+    include(valueClassEncodeTest(ShortWrapper(1)))
+    include(valueClassEncodeTest(BooleanWrapper(true)))
+    include(valueClassEncodeTest(FloatWrapper(1f)))
+    include(valueClassEncodeTest(DoubleWrapper(1.02)))
+    "encode referenced value class" {
 
-      val id = ValueClassSchemaTest.StringWrapper("100500")
-      val uuid = UUID.randomUUID()
-      val uuidStr = uuid.toString()
-      val uuidW = ValueClassSchemaTest.UuidWrapper(uuid)
-      val schema = Avro.default.schema(ValueClassSchemaTest.ContainsInlineTest.serializer())
-      Avro.default.toRecord(ValueClassSchemaTest.ContainsInlineTest.serializer(),
-         ValueClassSchemaTest.ContainsInlineTest(id, uuidW)) shouldBe ListRecord(schema, Utf8(id.a), Utf8(uuidStr))
-   }
-   
+        val id = ValueClassSchemaTest.StringWrapper("100500")
+        val uuid = UUID.randomUUID()
+        val uuidStr = uuid.toString()
+        val uuidW = ValueClassSchemaTest.UuidWrapper(uuid)
+        val schema = Avro.default.schema(ValueClassSchemaTest.ContainsInlineTest.serializer())
+        Avro.default.toAvroCompatibleType(
+            ValueClassSchemaTest.ContainsInlineTest.serializer(),
+            ValueClassSchemaTest.ContainsInlineTest(id, uuidW)
+        ) shouldBe ListRecord(schema, Utf8(id.a), Utf8(uuidStr))
+    }
+
 })
+
 @OptIn(InternalSerializationApi::class)
-fun valueClassEncodeTest(valueInstance : Any) = stringSpec {
-   "encode value class ${valueInstance::class.simpleName}" {      
-      Avro.default.toAvroCompatibleType(valueInstance::class.serializer() as KSerializer<Any>, valueInstance) shouldBe valueInstance   
-   }   
-   "encode value ${valueInstance::class.valueType}" {
-      val type = valueInstance::class.valueType
-      val actualValue = valueInstance::class.members.filterIsInstance<KProperty1<Any, *>>().first().get(valueInstance)
-      Avro.default.toAvroCompatibleType(type.serializer() as KSerializer<Any>, actualValue) shouldBe actualValue
-   }
+fun valueClassEncodeTest(valueInstance: Any) = stringSpec {
+    "encode value class ${valueInstance::class.simpleName}" {
+        Avro.default.toAvroCompatibleType(
+            valueInstance::class.serializer() as KSerializer<Any>,
+            valueInstance
+        ) shouldBe valueInstance
+    }
+    "encode value ${valueInstance::class.valueType}" {
+        val type = valueInstance::class.valueType
+        val actualValue = valueInstance::class.members.filterIsInstance<KProperty1<Any, *>>().first().get(valueInstance)
+        Avro.default.toAvroCompatibleType(type.serializer() as KSerializer<Any>, actualValue) shouldBe actualValue
+    }
 }
-private val KClass<*>.valueType : KClass<*> 
-   get() {
-      return this.members.filterIsInstance<KProperty<*>>().first().returnType.classifier as KClass<*>
-   }
+
+private val KClass<*>.valueType: KClass<*>
+    get() {
+        return this.members.filterIsInstance<KProperty<*>>().first().returnType.classifier as KClass<*>
+    }

2375c4b8cd293be1ad4e573c68a8c08f289b02cd
rutkowskij <jrutkowski.dev@gmail.com>
2025-02-02 08:58:33 +0100

    Make list wrapper benchmark usecase more real - using real structure in the list

--- benchmark/src/main/kotlin/com/github/avrokotlin/benchmark/internal/ListWrapperDataClass.kt
+++ benchmark/src/main/kotlin/com/github/avrokotlin/benchmark/internal/ListWrapperDataClass.kt
@@ -3,21 +3,31 @@ package com.github.avrokotlin.benchmark.internal
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ListWrapperDataClass(val parent: SimpleDataClass, val related: List<SimpleDataClass>) {
-    companion object {
-        fun create(size: Int) =
-            ListWrapperDataClass(SimpleDataClass.create(), List(size) { SimpleDataClass.create() })
-    }
+data class ListWrapperDataClass(val index: Int, val entries: List<StatsEntry>) {
+   companion object {
+      fun create(size: Int, entriesSize: Int) =
+         ListWrapperDataClass(RandomUtils.nextInt(), List(size) { StatsEntry.create(entriesSize) })
+   }
 }
 
+@Serializable
+data class StatsEntry(val elementId: ElementId, val values: List<StatValue>) {
+   companion object {
+      fun create(entriesSize: Int) =
+         StatsEntry(RandomUtils.nextLong(), List(entriesSize) { RandomUtils.nextLong() })
+   }
+}
+
+typealias ElementId = Long
+typealias StatValue = Long
 
 @Serializable
 data class ListWrapperDatasClass(
-    val data: List<ListWrapperDataClass>
+   val data: List<ListWrapperDataClass>
 ) {
-    companion object {
-        fun create(size: Int, wrapperSize: Int) =
-            ListWrapperDatasClass(data = List(size) { ListWrapperDataClass.create(wrapperSize) }
-            )
-    }
+   companion object {
+      fun create(size: Int, wrapperSize: Int, entriesSize: Int = 100) =
+         ListWrapperDatasClass(data = List(size) { ListWrapperDataClass.create(wrapperSize, entriesSize) }
+         )
+   }
 }

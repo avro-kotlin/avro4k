26749e2eba09f822bb9af0876848114be2cd691f
rutkowskij <jrutkowski.dev@gmail.com>
2025-02-04 09:59:13 +0100

    Fix RandomUtils issues, allow executing lists benchmarks separately

--- benchmark/src/main/kotlin/com/github/avrokotlin/benchmark/internal/ListWrapperDataClass.kt
+++ benchmark/src/main/kotlin/com/github/avrokotlin/benchmark/internal/ListWrapperDataClass.kt
@@ -3,31 +3,31 @@ package com.github.avrokotlin.benchmark.internal
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ListWrapperDataClass(val index: Int, val entries: List<StatsEntry>) {
-   companion object {
-      fun create(size: Int, entriesSize: Int) =
-         ListWrapperDataClass(RandomUtils.nextInt(), List(size) { StatsEntry.create(entriesSize) })
-   }
+internal data class ListWrapperDataClass(val index: Int, val entries: List<StatsEntry>) {
+    companion object {
+        fun create(size: Int, entriesSize: Int, random: RandomUtils = RandomUtils()) =
+            ListWrapperDataClass(random.nextInt(), List(size) { StatsEntry.create(entriesSize) })
+    }
 }
 
 @Serializable
-data class StatsEntry(val elementId: ElementId, val values: List<StatValue>) {
-   companion object {
-      fun create(entriesSize: Int) =
-         StatsEntry(RandomUtils.nextLong(), List(entriesSize) { RandomUtils.nextLong() })
-   }
+internal data class StatsEntry(val elementId: ElementId, val values: List<StatValue>) {
+    companion object {
+        fun create(entriesSize: Int, random: RandomUtils = RandomUtils()) =
+            StatsEntry(random.nextLong(), List(entriesSize) { random.nextLong() })
+    }
 }
 
 typealias ElementId = Long
 typealias StatValue = Long
 
 @Serializable
-data class ListWrapperDatasClass(
-   val data: List<ListWrapperDataClass>
+internal data class ListWrapperDatasClass(
+    val data: List<ListWrapperDataClass>
 ) {
-   companion object {
-      fun create(size: Int, wrapperSize: Int, entriesSize: Int = 100) =
-         ListWrapperDatasClass(data = List(size) { ListWrapperDataClass.create(wrapperSize, entriesSize) }
-         )
-   }
+    companion object {
+        fun create(size: Int, wrapperSize: Int, entriesSize: Int = 100) =
+            ListWrapperDatasClass(data = List(size) { ListWrapperDataClass.create(wrapperSize, entriesSize) }
+            )
+    }
 }

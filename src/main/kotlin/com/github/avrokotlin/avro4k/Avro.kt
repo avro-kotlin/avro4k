e679ceb0eee74508911201fea82d965f3bad3223
Chuckame <Chuckame@users.noreply.github.com>
2025-07-02 00:14:59 +0200

    chore: Improve documentation of Avro API

--- src/main/kotlin/com/github/avrokotlin/avro4k/Avro.kt
+++ src/main/kotlin/com/github/avrokotlin/avro4k/Avro.kt
@@ -25,7 +25,13 @@ import org.apache.avro.io.DecoderFactory
 import java.util.WeakHashMap
 
 /**
- * The goal of this class is to serialize and deserialize in avro binary format, not in GenericRecords.
+ * The goal of this class is to serialize and deserialize in avro binary format.
+ * That said, following the avro specification, each logical type is translated to a specific avro type and serialized as a specific byte sequence (strings in UTF8 bytes, int as zig-zag varint, etc).
+ *
+ * The main advantage of this library is that it allows you to serialize your data classes (and any other serializable type) to avro binary format, with the kotlin-style native serialization API provided by `kotlinx.serialization`.
+ * This framework maintained by jetbrains provides a powerful framework and gradle plugin to generate serializers at compile time, just with a few [kotlinx.serialization.Serializable] anotations.
+ *
+ * @sample com.github.avrokotlin.avro4k.internal.Samples.customizeAvroInstance
  */
 public sealed class Avro(
     public val configuration: AvroConfiguration,
@@ -43,6 +49,47 @@ public sealed class Avro(
             JavaTimeSerializersModule
     )
 
+    /**
+     * Infers the Avro schema for the given [descriptor].
+     *
+     * You may not need to call this method if you use the other encode/decode extension methods, as they will automatically generate the schema for you based on the serializer's descriptor.
+     *
+     * ## Types mapping
+     * - Classes and objects are translated as Avro records
+     * - Sealed classes are translated as UNION types
+     * - abstract classes must have their subclasses registered in the [Avro.serializersModule] to be properly translated
+     * - Enums are translated as Avro enums, taking into account the [AvroEnumDefault] annotation to set the default value
+     * - value classes are inlined as their underlying type
+     * - Primitive types such as [Boolean], [Int], [Long], [Float], [Double], and [String] are translated as their respective Avro types.
+     * - [Byte] and [Short] are translated as [Int] in Avro.
+     * - [UByte], [UShort], [UInt], and [ULong] are translated as their signed counterparts ([Int] and [Long]).
+     * - [ByteArray] is translated as BYTES type.
+     * - For the FIXED type, you can wrap a [ByteArray] in a value class, and annotate the underlying field with [AvroFixed]
+     * - [Map] is translated as a MAP type, where the keys are always converted from/to [String] (only for the primitive types)
+     * - Any other serializable [Iterable] is translated as an ARRAY type
+     * - Nullable types are translated as UNION types, with the null type being the first type in the union
+     * - [AvroDoc] annotation will be used to generate the Avro doc for a field or a named type (record, enum, fixed)
+     * - [AvroAlias] annotation will be used to set one or more aliases for a field or a named type (record, enum, fixed)
+     * - Annotate fields with [AvroStringable] to force the field type to be a string in the schema
+     * - Annotate a field with [AvroDefault] to set a default value for the field in the schema. The default value must be compatible with the field type.
+     * - Annotate a [java.math.BigDecimal] field with [AvroDecimal] to specify the scale and precision of the decimal avro logical type
+     * - Annotate a field or a type with [AvroProp] to add a custom property to the generate type's schema
+     *
+     * ## Cache details
+     * This method will cache the schema for the given descriptor, so subsequent calls with the same descriptor will return the cached schema.
+     * The cache is weakly referenced, meaning that it will not prevent the descriptor from being garbage collected.
+     * Also, the cache is not bounded, so calling this method with many different descriptors may lead to increased memory usage.
+     *
+     * ## Compatibility
+     * Do not forget to annotate evolving types and field names with [AvroAlias] to ensure compatibility with existing schemas and the used writer schemas.
+     *
+     * @param descriptor the descriptor for which to generate the schema.
+     * @return the Avro schema for the given descriptor.
+     *
+     * @see AvroConfiguration
+     * @see AvroBuilder.serializersModule
+     * @see com.github.avrokotlin.avro4k.serializer.AvroSerializer
+     */
     public fun schema(descriptor: SerialDescriptor): Schema {
         return schemaCache.getOrPut(descriptor) {
             lateinit var output: Schema
@@ -51,6 +98,29 @@ public sealed class Avro(
         }
     }
 
+    /**
+     * Encodes the given [value] to a [ByteArray] using the provided [writerSchema] and [serializer].
+     *
+     * You may prefer the other extension methods that infer the [serializer] and [writerSchema] based on a generic type [T].
+     *
+     * The given [serializer] must be "at least" compatible with the [writerSchema], meaning that any class field that is not present in the [writerSchema] will be ignored during encoding.
+     * Avro4k also match the [AvroAlias] of the classes and fields, and matches the [writerSchema]'s aliases to maximize the chances of compatibility with existing schemas.
+     * The schema can be generated using the [schema] method, or may be different, so that avro4k will adapt the value to the schema.
+     *
+     * ## Supported conversions
+     * - int types [Byte], [Short], [Int], [Long], their unsigned counterparts, and [Char] can be encoded INT or LONG
+     * - [Long] can be encoded to INT, but will fail if the value doesn't fit in the int range (same for unsigned types)
+     * - [Float] can be encoded to DOUBLE
+     * - [Double] can be encoded to FLOAT but will fail if the value doesn't fit in the float range
+     * - [String] can be encoded to any numerical type if the string is parsable
+     * - [String] can be encoded as UTF8 to BYTES and FIXED types, the later requiring the fixed size to match the actual string length
+     * - [String] can be encoded as ENUM
+     * - [Char] can be encoded as STRING
+     * - Classes and objects are encoded as RECORD, taking into account both [AvroAlias] and the [writerSchema]'s aliases to maximize compatibility
+     *
+     * @see AvroConfiguration
+     * @see AvroBuilder.serializersModule
+     */
     public fun <T> encodeToByteArray(
         writerSchema: Schema,
         serializer: SerializationStrategy<T>,
@@ -61,6 +131,22 @@ public sealed class Avro(
         return buffer.readByteArray()
     }
 
+    /**
+     * Decode the value from the given [bytes] to a value of type [T] assuming the data is respecting the given [writerSchema].
+     * If during the decoding, the written data does not match the [writerSchema], you may end up to unexpected results or an exception.
+     *
+     * You may prefer the other extension methods that infer the [deserializer] and [writerSchema] based on a generic type [T].
+     *
+     * For any custom encoding or conversion of type [T] or its inner type, you may need to register a custom [kotlinx.serialization.KSerializer] in [Avro.serializersModule].
+     *
+     * @param writerSchema the schema to use for decoding the value.
+     * @param deserializer the deserialization strategy to use for decoding the value. You may prefer the other extension methods without the deserializer parameter for convenience.
+     * @param bytes the [ByteArray] to read the encoded value from.
+     * @return the decoded value of type [T].
+     *
+     * @see AvroConfiguration
+     * @see AvroBuilder.serializersModule
+     */
     public fun <T> decodeFromByteArray(
         writerSchema: Schema,
         deserializer: DeserializationStrategy<T>,
@@ -74,6 +160,30 @@ public sealed class Avro(
         return result
     }
 
+    /**
+     * Decode the value from the given [bytes] to a value of type [T] assuming the data is respecting the given [writerSchema].
+     * If during the decoding, the written data does not match the [writerSchema], you may end up to unexpected results or an exception.
+     *
+     * You may prefer the other extension methods that infer the [serializer] based on a generic type [T].
+     *
+     * For any custom encoding or conversion of type [T] or its inner type, you may need to register a custom [kotlinx.serialization.KSerializer] in [Avro.serializersModule].
+     *
+     * ## Supported conversions
+     * - LONG and INT types can be decoded into [Byte], [Short], [Int], [Long], their unsigned counterparts, and [Char] (if the value fits in the type range)
+     * - FLOAT and DOUBLE types can be decoded into [Float] and [Double] (if the value fits in the type range)
+     * - STRING can be decoded into any numerical type, as long as the string is parsable
+     * - STRING can be decoded to [Char], as long as the string is a single character
+     * - RECORD can be decoded to classes and objects, taking into account both [AvroAlias] and the [writerSchema]'s aliases to maximize compatibility
+     * - ENUM can be decoded to [String] or [Enum] types, as long as the enum value is present in the schema
+     *
+     * @param deserializer the deserialization strategy to use for decoding the value. You may prefer the other extension methods without the deserializer parameter for convenience.
+     * @param bytes the [ByteArray] to read the encoded value from.
+     * @return the decoded value of type [T].
+     *
+     * @see AvroConfiguration
+     * @see Avro.schema
+     * @see AvroBuilder.serializersModule
+     */
     override fun <T> decodeFromByteArray(
         deserializer: DeserializationStrategy<T>,
         bytes: ByteArray,
@@ -81,6 +191,12 @@ public sealed class Avro(
         return decodeFromByteArray(schema(deserializer.descriptor), deserializer, bytes)
     }
 
+    /**
+     * Encodes the given [value] to a [ByteArray] using the provided [serializer].
+     * The schema is inferred from the [serializer]'s descriptor, and the value is encoded using that schema.
+     *
+     * You may prefer the other extension methods that infer the [serializer] based on a generic type [T].
+     */
     override fun <T> encodeToByteArray(
         serializer: SerializationStrategy<T>,
         value: T,
@@ -99,17 +215,36 @@ public fun Avro(
 }
 
 public class AvroBuilder internal constructor(avro: Avro) {
+    /**
+     * @see AvroConfiguration.fieldNamingStrategy
+     */
     @ExperimentalSerializationApi
     public var fieldNamingStrategy: FieldNamingStrategy = avro.configuration.fieldNamingStrategy
 
+    /**
+     * @see AvroConfiguration.implicitNulls
+     */
     @ExperimentalSerializationApi
     public var implicitNulls: Boolean = avro.configuration.implicitNulls
 
+    /**
+     * @see AvroConfiguration.implicitEmptyCollections
+     */
     @ExperimentalSerializationApi
     public var implicitEmptyCollections: Boolean = avro.configuration.implicitEmptyCollections
 
+    /**
+     * @see AvroConfiguration.validateSerialization
+     */
     @ExperimentalSerializationApi
     public var validateSerialization: Boolean = avro.configuration.validateSerialization
+
+    /**
+     * The serializers module to use for encoding, decoding, and inferring schemas from kotlin types.
+     *
+     * @see KSerializer
+     * @see com.github.avrokotlin.avro4k.serializer.AvroSerializer
+     */
     public var serializersModule: SerializersModule = EmptySerializersModule()
 
     internal fun build(): AvroConfiguration =
@@ -124,15 +259,30 @@ public class AvroBuilder internal constructor(avro: Avro) {
 private class AvroImpl(configuration: AvroConfiguration, serializersModule: SerializersModule) :
     Avro(configuration, serializersModule)
 
+/**
+ * Infers the Avro schema for the given type [T].
+ *
+ * @see Avro.schema
+ */
 public inline fun <reified T> Avro.schema(): Schema {
     val serializer = serializersModule.serializer<T>()
     return schema(serializer.descriptor)
 }
 
+/**
+ * Infers the Avro schema for the given [serializer].
+ *
+ * @see Avro.schema
+ */
 public fun <T> Avro.schema(serializer: KSerializer<T>): Schema {
     return schema(serializer.descriptor)
 }
 
+/**
+ * Encodes the given [value] to a [ByteArray] using the provided [writerSchema].
+ *
+ * @see Avro.encodeToByteArray
+ */
 public inline fun <reified T> Avro.encodeToByteArray(
     writerSchema: Schema,
     value: T,
@@ -141,6 +291,14 @@ public inline fun <reified T> Avro.encodeToByteArray(
     return encodeToByteArray(writerSchema, serializer, value)
 }
 
+/**
+ * Decode the value from the given [bytes] to a value of type [T] assuming the data is respecting the [writerSchema].
+ * If during the decoding, the written data does not match [T]'s schema, you may end up to unexpected results or an exception.
+ *
+ * For any custom encoding or conversion of type [T] or its inner type, you may need to register a custom [kotlinx.serialization.KSerializer] in [Avro.serializersModule].
+ *
+ * @see Avro.decodeFromByteArray
+ */
 public inline fun <reified T> Avro.decodeFromByteArray(
     writerSchema: Schema,
     bytes: ByteArray,

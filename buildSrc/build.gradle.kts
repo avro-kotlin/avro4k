b28c77e039e164ad10b1ee4e32b926a73ee1236d
Chuckame <Chuckame@users.noreply.github.com>
2025-08-30 01:09:49 +0200

    build: try fixing release again

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.18.1")
 
     implementation("com.diffplug.spotless:com.diffplug.spotless.gradle.plugin:7.2.1")
-    implementation("io.github.gradle-nexus.publish-plugin:io.github.gradle-nexus.publish-plugin.gradle.plugin:2.0.0")
+
+    implementation("com.vanniktech.maven.publish:com.vanniktech.maven.publish.gradle.plugin:0.34.0")
 
     implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.0.0")
     implementation("org.jetbrains.dokka-javadoc:org.jetbrains.dokka-javadoc.gradle.plugin:2.0.0")

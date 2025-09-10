89d0e6c0d83f76f6f6265f4e8122c0c21300dcfc
Chuckame <Chuckame@users.noreply.github.com>
2025-09-10 23:54:08 +0200

    feat: Add gradle plugin to generate kotlin sources (#370)

---
+++ example/kotlin-plugin/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+    id("com.github.avro-kotlin.avro4k")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
---
+++ gradle-plugin/src/test/kotlin/com/github/avrokotlin/avro4k/plugin/gradle/Avro4kGradlePluginTest.kt
@@ -0,0 +1,45 @@
+package com.github.avrokotlin.avro4k.plugin.gradle
+
+import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldBeIn
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Avro4kGradlePluginTest {
+    @Test
+    fun `plugin generates kotlin classes and compiles`() {
+        val projectDir = File("../example/kotlin-plugin").canonicalFile
+        File(projectDir, "build").deleteRecursively()
+
+        // Run build
+        val result =
+            GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withArguments("compileKotlin")
+                .withPluginClasspath() // this adds gradle-plugin to classpath
+                .forwardOutput()
+                .withDebug(true)
+                .build()!!
+
+        // compileKotlin should generate avro sources, and also compile them
+        result.shouldHaveTaskSuccessOrUpToDate(":generateAvroKotlinSources")
+        result.shouldHaveTaskSuccessOrUpToDate(":compileAvroKotlin")
+        result.shouldHaveTaskSuccessOrUpToDate(":avroClasses")
+        result.shouldHaveTaskSuccessOrUpToDate(":compileKotlin")
+
+        // Check generated file exists
+        assertTrue(File(projectDir, "build/generated/sources/avro/main/MyKeySchema.kt").exists())
+        assertTrue(File(projectDir, "build/generated/sources/avro/main/example/avro4k/MyValueSchema.kt").exists())
+    }
+
+    fun BuildResult.shouldHaveTaskSuccessOrUpToDate(taskPath: String) {
+        val task = this.task(taskPath)
+        withClue("'$taskPath' task should succeed or be up-to-date") {
+            task?.outcome shouldBeIn listOf(TaskOutcome.SUCCESS, TaskOutcome.UP_TO_DATE)
+        }
+    }
+}

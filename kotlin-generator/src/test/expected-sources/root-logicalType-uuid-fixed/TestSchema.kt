3a8995bb1ca9324e8a434c9a7a263bdcdc04e6c9
Chuckame <Chuckame@users.noreply.github.com>
2025-11-12 14:42:32 +0100

    feat(generator): Add fixed uuid source generation

---
+++ kotlin-generator/src/test/expected-sources/root-logicalType-uuid-fixed/TestSchema.kt
@@ -0,0 +1,25 @@
+@file:OptIn(
+    InternalAvro4kApi::class,
+    ExperimentalAvro4kApi::class,
+)
+
+import com.github.avrokotlin.avro4k.AvroFixed
+import com.github.avrokotlin.avro4k.AvroProp
+import com.github.avrokotlin.avro4k.ExperimentalAvro4kApi
+import com.github.avrokotlin.avro4k.InternalAvro4kApi
+import com.github.avrokotlin.avro4k.`internal`.AvroGenerated
+import com.github.avrokotlin.avro4k.serializer.UUIDSerializer
+import java.util.UUID
+import kotlin.OptIn
+import kotlin.jvm.JvmInline
+import kotlinx.serialization.Serializable
+
+@JvmInline
+@Serializable
+@AvroGenerated("""{"type":"fixed","name":"TheUuid","namespace":"ns","size":16,"logicalType":"uuid"}""")
+public value class TestSchema(
+    @AvroFixed(size = 16)
+    @AvroProp("logicalType", "uuid")
+    @Serializable(with = UUIDSerializer::class)
+    public val `value`: UUID,
+)

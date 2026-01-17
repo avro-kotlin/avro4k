4ab3903e9073021541c7d5a83ee9a0b529e43685
Lee Won Jun <dldnjs1013@nate.com>
2026-01-17 17:34:01 +0900

    docs: clarify FieldNamingStrategy KDoc
    Co-authored-by: Chuckame <Chuckame@users.noreply.github.com>

--- gradle-plugin/src/main/kotlin/com/github/avrokotlin/avro4k/plugin/gradle/FieldNamingStrategyType.kt
+++ gradle-plugin/src/main/kotlin/com/github/avrokotlin/avro4k/plugin/gradle/FieldNamingStrategyType.kt
@@ -1,7 +1,8 @@
 package com.github.avrokotlin.avro4k.plugin.gradle
 
 /**
- * Enum representing the available field naming strategies for the Gradle plugin.
+ * Enum representing the available naming strategies for the Gradle plugin,
+ * used to map the original record fields names to the kotlin properties names during source generation.
  */
 public enum class FieldNamingStrategyType {
     IDENTITY,

6b23e1e2b89875995af681ebebe5b4c99e2ae09e
Lee Won Jun <dldnjs1013@nate.com>
2026-01-17 17:34:44 +0900

    docs: add KDoc to FieldNamingStrategyType values
    Co-authored-by: Chuckame <Chuckame@users.noreply.github.com>

--- gradle-plugin/src/main/kotlin/com/github/avrokotlin/avro4k/plugin/gradle/FieldNamingStrategyType.kt
+++ gradle-plugin/src/main/kotlin/com/github/avrokotlin/avro4k/plugin/gradle/FieldNamingStrategyType.kt
@@ -5,6 +5,16 @@ package com.github.avrokotlin.avro4k.plugin.gradle
  * used to map the original record fields names to the kotlin properties names during source generation.
  */
 public enum class FieldNamingStrategyType {
+    /**
+     * Naming strategy that is basically returning the original avro field name.
+     *
+     * This is the default strategy.
+     */
     IDENTITY,
+    /**
+     * Naming strategy that formats the original avro field name to its camel-case counterpart.
+     *
+     * Example: `user_v1UUID` is formatted as `userV1Uuid`.
+     */
     CAMEL_CASE,
 }
